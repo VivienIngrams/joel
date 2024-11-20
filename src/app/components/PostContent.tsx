00e6ce4869481a2e6bd3c23045962cde200bdb7b
VivienIngrams <vivingrams@gmail.com>
2024-11-20 15:17:55 +0000

    Text z index

--- src/app/components/PostContent.tsx
+++ src/app/components/PostContent.tsx
@@ -10,7 +10,7 @@ interface PostContentProps {
 
 function PostContent({ post }: PostContentProps) {
   return (
-    <div className="w-full h-full md:fixed md:max-w-[20vw] md:top-0 md:left-0 md:bg-gradient-to-r from-black to-transparent pt-16 pb-6 md:py-24 px-4 md:px-7  md:text-left z-50">
+    <div className="w-full h-full md:fixed md:max-w-[20vw] md:top-0 md:left-0 md:bg-gradient-to-r from-black to-transparent pt-16 pb-6 md:py-24 px-4 md:px-7  md:text-left z-40">
       <h1 className="text-2xl font-bold">{post.title}</h1>
       
       {post.excerpt && (
