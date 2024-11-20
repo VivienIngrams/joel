048564c84f0caddc50f3451cbb83189de74248e3
VivienIngrams <vivingrams@gmail.com>
2024-11-20 14:19:32 +0000

    Unable to overlay over texts and container

--- src/app/components/HorizontalScroll.tsx
+++ src/app/components/HorizontalScroll.tsx
@@ -1,61 +1,65 @@
-'use client';
+'use client'
 
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import Image from 'next/legacy/image';
-import { usePathname } from 'next/navigation';
-import React, { useEffect, useRef, useState } from 'react';
-
-import { urlForImage } from '~/sanity/lib/sanity.image';
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import Image from 'next/legacy/image'
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useRef, useState } from 'react'
+import { urlForImage } from '~/sanity/lib/sanity.image'
 
 interface HorizontalGalleryProps {
-  mainImages: any[];
-  layout: 'portrait' | 'landscape' | 'square'; // Define the layout type as a specific string union
+  mainImages: any[]
+  layout: 'portrait' | 'landscape' | 'square'
 }
 
-export function HorizontalScroll({ mainImages, layout }: HorizontalGalleryProps) {
-  const sectionRef = useRef<HTMLDivElement | null>(null);
-  const triggerRef = useRef<HTMLDivElement | null>(null);
-  const [dimensions, setDimensions] = useState({ height: 0, totalImagesWidth: 0 });
+export function HorizontalScroll({
+  mainImages,
+  layout,
+}: HorizontalGalleryProps) {
+  const sectionRef = useRef<HTMLDivElement | null>(null)
+  const triggerRef = useRef<HTMLDivElement | null>(null)
+  const [dimensions, setDimensions] = useState({
+    height: 0,
+    totalImagesWidth: 0,
+  })
+  const [selectedImage, setSelectedImage] = useState<string | null>(null)
 
-  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger)
 
-  const path = usePathname();
-  const isMainPostsPage = path === '/posts';
+  const path = usePathname()
+  const isMainPostsPage = path === '/posts'
 
   useEffect(() => {
-    // Only execute if window is defined (client-side)
     if (typeof window !== 'undefined') {
-      const height = window.innerHeight * 0.78; 
-      let totalImagesWidth = 0;
+      const height = window.innerHeight * 0.78
+      let totalImagesWidth = 0
 
-      // Calculate total width based on layout property
       const imageWidths = mainImages.map(() => {
-        let width;
+        let width
         switch (layout) {
           case 'portrait':
-            width = height * (3 / 4); // 3:4 aspect ratio
-            break;
+            width = height * (3 / 4) // 3:4 aspect ratio
+            break
           case 'landscape':
-            width = height * (16 / 9); // 16:9 aspect ratio
-            break;
+            width = height * (16 / 9) // 16:9 aspect ratio
+            break
           case 'square':
           default:
-            width = height; // 1:1 aspect ratio for square or default
-            break;
+            width = height // 1:1 aspect ratio for square or default
+            break
         }
-        totalImagesWidth += width;
-        return width;
-      });
+        totalImagesWidth += width
+        return width
+      })
 
-      setDimensions({ height, totalImagesWidth });
+      setDimensions({ height, totalImagesWidth })
     }
-  }, [mainImages, layout]);
+  }, [mainImages, layout])
 
   useEffect(() => {
     if (dimensions.totalImagesWidth > 0 && typeof window !== 'undefined') {
-      const containerWidth = window.innerWidth * 0.7;
-      const totalWidth = dimensions.totalImagesWidth - containerWidth;
+      const containerWidth = window.innerWidth * 0.7
+      const totalWidth = dimensions.totalImagesWidth - containerWidth
 
       const pin = gsap.fromTo(
         sectionRef.current,
@@ -66,24 +70,43 @@ export function HorizontalScroll({ mainImages, layout }: HorizontalGalleryProps)
           scrollTrigger: {
             trigger: triggerRef.current,
             start: 'center center',
-            end: `${totalWidth} top`, // Adjust this based on the expected scroll length
+            end: `${totalWidth} top`,
             scrub: true,
             pin: true,
-            // markers: true, // Remove this in production
           },
         }
-      );
+      )
 
       return () => {
-        pin.kill();
-      };
+        pin.kill()
+      }
     }
-  }, [dimensions]);
+  }, [dimensions])
+
+  // Close the modal if clicked outside the image
+  const handleClickOutside = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      setSelectedImage(null)
+    }
+  }
+
+  useEffect(() => {
+    // Disable scroll when modal is open
+    if (selectedImage) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  }, [selectedImage])
 
   return (
     <section
       ref={triggerRef}
-      className={`w-full h-full pt-16 overflow-hidden bg-neutral-800 ${!isMainPostsPage ? 'pl-[20vw]' : ''}`}
+      className={`w-full h-full pt-16 overflow-hidden bg-neutral-800 ${!isMainPostsPage ? 'pl-[20vw] 2xl:pl-[20vw]' : ''}`}
     >
       <div
         ref={sectionRef}
@@ -91,16 +114,25 @@ export function HorizontalScroll({ mainImages, layout }: HorizontalGalleryProps)
         style={{ width: `${dimensions.totalImagesWidth}px` }}
       >
         {mainImages.map((image, index) => {
-          const width = dimensions.height * (layout === 'portrait' ? (3 / 4) : layout === 'landscape' ? (16 / 9) : 1);
+          const width =
+            dimensions.height *
+            (layout === 'portrait'
+              ? 3 / 4
+              : layout === 'landscape'
+              ? 16 / 9
+              : 1)
 
           return (
             <div
               key={image._key || index.toString()}
-              className="relative flex-shrink-0"
+              className="relative flex-shrink-0 cursor-pointer"
               style={{
                 width: `${width}px`,
                 height: `${dimensions.height}px`,
               }}
+              onClick={() =>
+                setSelectedImage(urlForImage(image).url() as string)
+              } // Open modal on click
             >
               <Image
                 src={urlForImage(image).url() as string}
@@ -108,14 +140,41 @@ export function HorizontalScroll({ mainImages, layout }: HorizontalGalleryProps)
                 alt={image.alt || `Image ${index + 1}`}
                 layout="fill"
                 objectFit="cover"
-                className='mt-24'
+                className="mt-24"
               />
             </div>
-          );
+          )
         })}
       </div>
+
+      {/* Modal for viewing the image in full */}
+      {selectedImage && (
+        <div
+          className="fixed top-0 left-0 w-screen h-screen z-[99999] flex items-center justify-center bg-black/90"
+          style={{ overflow: 'hidden' }} // Prevent scrolling inside modal
+          onClick={handleClickOutside} // Close modal if clicked outside the image
+        >
+          <div className="relative w-auto h-auto max-w-[90vw] max-h-[99vh] flex items-center justify-center">
+            <Image
+              src={selectedImage}
+              alt="Full View"
+              layout="intrinsic"
+              width={1200}
+              height={800}
+              className="rounded-lg"
+              objectFit="contain"
+            />
+            <button
+              className="z-[99999] absolute top-20 right-5 text-white bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-70"
+              onClick={() => setSelectedImage(null)} // Close modal
+            >
+              ✕
+            </button>
+          </div>
+        </div>
+      )}
     </section>
-  );
+  )
 }
 
-export default HorizontalScroll;
+export default HorizontalScroll
