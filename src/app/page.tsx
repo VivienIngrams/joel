62a2484e81f471b9d524276b17c470bce8b78c21
VivienIngrams <vivingrams@gmail.com>
2024-11-29 11:39:26 +0000

    NEw Homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,21 +19,21 @@ export default async function HomePage() {
 
   return (
     <Link href="/posts" className="block relative h-[100vh] w-full">
-      <section className="relative h-full w-full flex flex-col justify-start md:justify-center p-4 md:p-6 items-start pb-[55px] md:pb-[75px] overflow-x-hidden overflow-y-hidden">
+      <section className="relative h-full w-full flex flex-col justify-start md:justify-center p-4 md:pt-4 items-start pb-[95px]  md:p-[110px] overflow-x-hidden overflow-y-hidden">
         {/* Age Verification Popup */}
         <Verification />
 
         {/* Content Container */}
-        <div className="relative flex-1 w-full border-white border-2 shadow-lg flex overflow-hidden max-w-full h-full">
+        <div className="relative flex-1 w-full border-white border-2 shadow-lg shadow-gray-600 flex overflow-hidden max-w-full h-full">
           {/* Slider */}
-          <div className="w-full h-full">
+          <div className="w-full h-full ">
             <Slider images={homePageData?.mainImages} />
           </div>
         </div>
 
         {/* Title and Subtitle */}
         <div className="absolute bottom-2 inset-x-0 text-center px-4 pointer-events-none">
-          <h1 className="uppercase text-white text-5xl md:text-[100px] mb-[-12px] break-words leading-tight">
+          <h1 className="uppercase text-white text-5xl md:text-[80px] mb-[-16px] break-words leading-tight">
             Joël Bardeau
           </h1>
           <h2 className="text-white text-2xl md:text-3xl font-light leading-normal">
