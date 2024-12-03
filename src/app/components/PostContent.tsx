568b0eea97f3f98df17176c028440aa309c8b18d
VivienIngrams <vivingrams@gmail.com>
2024-12-03 16:09:22 +0000

    Checks

--- src/app/components/PostContent.tsx
+++ src/app/components/PostContent.tsx
@@ -1,5 +1,7 @@
 import { PortableText } from '@portabletext/react'
 import { PortableTextBlock } from '@portabletext/types'
+import { FaArrowDown } from "react-icons/fa6";
+
 
 interface PostContentProps {
   post: {
@@ -11,8 +13,8 @@ interface PostContentProps {
 function PostContent({ post }: PostContentProps) {
   return (
     <>
-      <div className="w-full h-full md:fixed md:max-w-[30vw] min-[1800px]:max-w-[27vw] md:top-0 md:left-0 md:bg-gradient-to-r from-[#818895] via-[#818895]/90 to-transparent pt-16 pb-6 md:py-16 px-4 md:px-7 z-40">
-        <div className="md:h-[80vh] flex flex-col justify-start md:justify-center">
+      <div className="w-full md:fixed md:max-w-[30vw] min-[1800px]:max-w-[27vw] md:top-0 md:left-0 md:bg-gradient-to-r from-[#818895] via-[#818895]/90 to-transparent pt-12 pb-6 md:py-16 md:px-10 z-40">
+        <div className="md:h-[70vh] flex flex-col justify-start md:justify-center">
           {/* Title */}
           <h1 className="text-3xl font-barlow  md:text-4xl tracking-tighter font-light uppercase ">
             {post.title}
@@ -28,9 +30,15 @@ function PostContent({ post }: PostContentProps) {
       </div>
       {/* Excerpt Desktop*/}
       {post.excerpt && (
-        <div className=" hidden md:block text-xl text-justify max-w-2xl mb-8">
+        <>
+        <div className="mt-16 hidden md:block text-xl text-justify max-w-[450px] mb-8">
           <PortableText value={post.excerpt} />
         </div>
+        <div className='hidden md:block h-12 mt-12'>
+          <FaArrowDown  className="text-white text-[1.5rem]" />
+        </div>
+        </>
+        
       )}
     </>
   )
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from 'next/navigation';
+import { notFound } from 'next/navigation'
 
 import HorizontalScroll from '~/app/components/HorizontalScroll'
 import MobileScroll from '~/app/components/MobileScroll'
@@ -17,13 +17,12 @@ export default async function PostPage({
   // Fetch the post by slug
   const post: Post | null = await getPost(client, params.slug, {
     next: {
-      revalidate: 1, 
+      revalidate: 1,
       cache: 'no-store',
-
     },
   })
 
-console.log(post)
+  console.log(post)
   // Handle case where no post is found
   if (!post) {
     return <p>No post found.</p>
@@ -31,27 +30,19 @@ console.log(post)
 
   return (
     <>
-       
-     
-      <div className="h-screen w-screen flex flex-col items-center justify-center bg-[#818895] text-white px-6 py-12">
-       
-       
-       <PostContent post={post} />
-     </div>
-    <div className="relative min-h-[80vh] md:h-full w-screen flex flex-col justify-center md:justify-start md:flex-row">
- 
-      {/* // Mobile View */}
-      <div className="h-full w-full md:hidden mb-20">
-        <MobileScroll
-          images={post.images}
-          title={post.title}
-        />
+      <div className="md:h-screen w-screen flex flex-col items-center justify-center bg-[#818895] text-white px-6 py-12">
+        <PostContent post={post} />
       </div>
-      {/* Horizontal Scrolling Image Gallery on the Right */}
-      <div className="hidden overflow-hidden md:block md:flex-grow md:pl-4 h-full">
-        <HorizontalScroll images={post.images} title={post.title}/>
+      <div className="relative  md:h-full w-screen flex flex-col justify-center md:justify-start md:flex-row">
+        {/* // Mobile View */}
+        <div className="h-full w-full md:hidden mb-20">
+          <MobileScroll images={post.images} title={post.title} />
+        </div>
+        {/* Horizontal Scrolling Image Gallery on the Right */}
+        <div className="hidden overflow-hidden md:block md:flex-grow md:pl-4 h-full">
+          <HorizontalScroll images={post.images} title={post.title} />
+        </div>
       </div>
-    </div>
     </>
   )
 }
