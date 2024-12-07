d287f8e4dc2423f110e3e697e9b9f021be945ae9
VivienIngrams <vivingrams@gmail.com>
2024-12-07 13:00:25 +0000

    INfo page

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -6,32 +6,44 @@ import { Socials } from '../components/Container'
 
 export default function InfoPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-[#4b5563] text-white px-6 py-12">
-      <Link
-        href="mailto:info@joelbardeau.com"
-        className="text-3xl md:text-5xl upper font-light  mb-4"
-      >
-        Contact
-      </Link>
-      <p className="text-lg md:text-xl text-center max-w-2xl mb-8">
-        Jeune homme j&apos;ai suivi les cours classiques de dessin aux
-        Beaux-Arts et pratiqué assidûment la photographie argentique. Puis une
-        carrière dans le monde médico-chirurgical. Aujourd&apos;hui plus de 30
-        expositions au compteur, mes travaux sont présents aux murs de nombreux
-        collectionneurs. « Ma démarche est tournée principalement vers l’humain.
-        Au travers de son enveloppe j’aime chercher et creuser le MOI des êtres.
-        Mon travail en autoportraits est une forme d’introspection à la fois
-        thérapeutique et artistique ».
-      </p>
-      <Link
-        href="mailto:info@joelbardeau.com"
-        className="text-xl md:text-xl text-center max-w-2xl mb-8  hover:text-neutral-500"
-      >
-        info@joelbardeau.com
-      </Link>
+    <div className="min-h-screen md:grid md:grid-cols-3 bg-[#4b5563] text-white md:mx-[10vw]">
+      <div className="min-h-screen flex flex-col items-center justify-center px-6   py-12">
+        <Link
+          href="mailto:info@joelbardeau.com"
+          className="text-3xl md:text-5xl upper font-light  mb-4 md:mb-8"
+        >
+          Contact
+        </Link>
+        <Link
+          href="mailto:info@joelbardeau.com"
+          className="text-xl md:text-xl text-center max-w-2xl mb-4 md:mb-8  hover:text-neutral-500"
+        >
+          info@joelbardeau.com
+        </Link>
 
-      <div className="flex items-end gap-x-4">
-        <Socials />
+        <div className="flex items-end gap-x-4">
+          <Socials />
+        </div>
+      </div>
+      <div className="min-h-screen flex flex-col items-center justify-center px-6   py-12">
+
+      <div className='bg-black h-40 w-24'></div></div>
+      <div className="min-h-screen flex flex-col items-center justify-center text-lg md:text-xl text-justify max-w-2xl px-6 md:px-16 py-12">
+        <p className=" mb-4">
+          Jeune homme j&apos;ai suivi les cours classiques de dessin aux
+          Beaux-Arts et pratiqué assidûment la photographie argentique. Puis une
+          carrière dans le monde médico-chirurgical.
+        </p>
+        <p className=" mb-4">
+          Aujourd&apos;hui plus de 30 expositions au compteur, mes travaux sont
+          présents aux murs de nombreux collectionneurs.
+        </p>
+        <p className="italic mb-4">
+          « Ma démarche est tournée principalement vers l&apos;humain. Au travers de
+          son enveloppe j&apos;aime chercher et creuser le MOI des êtres. Mon travail
+          en autoportraits est une forme d&apos;introspection à la fois thérapeutique
+          et artistique ».
+        </p>
       </div>
     </div>
   )
