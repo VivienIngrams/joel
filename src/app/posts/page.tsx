0537a5156bab5b780d330ce6701d7e78c60de02d
VivienIngrams <vivingrams@gmail.com>
2024-12-10 14:58:21 +0000

    Improving block content

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -11,7 +11,7 @@ export default async function PostsPage() {
 
   const posts: Post[] = await getPosts(client, {
     next: {
-      revalidate: 30,
+      revalidate: 100,
     },
   })
 
--- src/sanity/schemas/blockContent.ts
+++ src/sanity/schemas/blockContent.ts
@@ -42,7 +42,7 @@ export default defineType({
       title: 'Hard Break',
       type: 'object',
       name: 'hardBreak',
-      
+      fields: [],
       preview: {
         prepare() {
           return {
