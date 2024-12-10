086ef3e60cb84f42bb9db0c30996f9566fb160f2
VivienIngrams <vivingrams@gmail.com>
2024-12-10 15:21:34 +0000

    Improving block content

--- src/sanity/schemas/blockContent.ts
+++ src/sanity/schemas/blockContent.ts
@@ -1,55 +1,40 @@
-import { defineArrayMember, defineType } from 'sanity';
+import { defineArrayMember, defineType } from 'sanity'
 
+/**
+ * This is the schema definition for the rich text fields used for
+ * for this blog studio. When you import it in schemas.js it can be
+ * reused in other parts of the studio with:
+ *  {
+ *    name: 'someName',
+ *    title: 'Some title',
+ *    type: 'blockContent'
+ *  }
+ */
 export default defineType({
   title: 'Block Content',
   name: 'blockContent',
   type: 'array',
   of: [
     defineArrayMember({
-      title: 'Block',
       type: 'block',
-      styles: [
-        { title: 'Normal', value: 'normal' },
-        { title: 'H1', value: 'h1' },
-        { title: 'H2', value: 'h2' },
-        { title: 'H3', value: 'h3' },
-        { title: 'H4', value: 'h4' },
-        { title: 'Quote', value: 'blockquote' },
-      ],
-      lists: [{ title: 'Bullet', value: 'bullet' }],
       marks: {
-        decorators: [
-          { title: 'Strong', value: 'strong' },
-          { title: 'Emphasis', value: 'em' },
-        ],
         annotations: [
           {
-            title: 'URL',
             name: 'link',
             type: 'object',
+            title: 'Link',
             fields: [
               {
-                title: 'URL',
                 name: 'href',
                 type: 'url',
+                title: 'Url',
               },
             ],
           },
         ],
       },
+      styles: [],
     }),
-    defineArrayMember({
-      title: 'Hard Break',
-      type: 'object',
-      name: 'hardBreak',
-      fields: [],
-      preview: {
-        prepare() {
-          return {
-            title: '<br/> (Line Break)',
-          };
-        },
-      },
-    }),
+      
   ],
-});
+})
