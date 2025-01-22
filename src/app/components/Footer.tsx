a91111db95c82ffd1d42966327ce5d49505980ef
VivienIngrams <vivingrams@gmail.com>
2025-01-22 21:21:52 +0000

    Fixed footer link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,16 @@
-'use client';
+'use client'
 
-import Link from 'next/link';
-import NavMenu from './NavMenu';
-import Socials from './Socials';
+import Link from 'next/link'
+import NavMenu from './NavMenu'
+import Socials from './Socials'
 
-export default function Footer({ isHomePage, isContactPage }: { isHomePage: boolean; isContactPage: boolean }) {
+export default function Footer({
+  isHomePage,
+  isContactPage,
+}: {
+  isHomePage: boolean
+  isContactPage: boolean
+}) {
   return (
     <footer
       className={`fixed bottom-0 w-full md:h-8 p-4 pt-4 md:pb-7 flex items-end md:items-center justify-between ${
@@ -23,18 +29,10 @@ export default function Footer({ isHomePage, isContactPage }: { isHomePage: bool
       ></div>
 
       {/* Website Link */}
-      <div
-        className={` ${
-          isContactPage ? 'block' : 'hidden'
-        }`}
-      >
+      <div className={` `}>
         <Link
           href="https://vivieningrams.com"
-          className={`font-arsenal text-gray-500 hover:text-black tracking-normal leading-loose ${
-            isContactPage
-              ? 'fixed right-4 bottom-12 md:relative md:left-2 md:bottom-0 text-[12px]'
-              : ''
-          }`}
+          className={`${isContactPage ? 'block' : 'hidden'} font-arsenal text-gray-500 hover:text-black tracking-normal leading-loose text-[12px] ml-12 md:ml-2 mb-1  }`}
         >
           Website by Vivien Ingrams
         </Link>
@@ -43,7 +41,7 @@ export default function Footer({ isHomePage, isContactPage }: { isHomePage: bool
       <NavMenu />
 
       {/* Social Icons */}
-      <div className="z-50">
+      <div className=" z-50">
         {!isHomePage && (
           <div className="flex items-end mb-2 gap-x-3">
             <div className="flex gap-x-3">
@@ -53,5 +51,5 @@ export default function Footer({ isHomePage, isContactPage }: { isHomePage: bool
         )}
       </div>
     </footer>
-  );
+  )
 }
